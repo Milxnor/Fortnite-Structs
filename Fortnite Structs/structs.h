7d526d3ed2c4af99053bd9ff062db286b7166ba5
Milxnor <dsicordalt192@gmail.com>
2022-04-30 11:23:41 -0400

    fix 15.30

--- Fortnite Structs/structs.h
+++ Fortnite Structs/structs.h
@@ -727,7 +727,9 @@ bool Setup(/* void* ProcessEventHookAddr */)
 			ObjectsAddr = FindPattern(_("48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1"), true, 3);
 	}
 
-	if (Engine_Version >= 426) // 4.26.1
+	auto FnVerDouble = std::stod(FN_Version);
+
+	if (FnVerDouble >= 16.00) // 4.26.1
 	{
 		ToStringAddr = FindPattern(_("48 89 5C 24 ? 48 89 74 24 ? 55 57 41 56 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 45 33 F6 48 8B F2 44 39 71 04 0F 85 ? ? ? ? 8B 19 0F B7 FB E8 ? ? ? ? 8B CB 48 8D 54 24 ? 48 C1 E9 10 8D 1C 3F 48 03 5C C8 ? 48 8B CB F6 03 01 0F 85 ? ? ? ? E8 ? ? ? ?"));
 		ObjectsAddr = FindPattern(_("48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1"), true, 3);
